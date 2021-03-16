1c35587da1d4d1f73a1e942879189a37adcf1243
PAIN <pkostirev1@gmail.com>
2021-03-17 01:05:50 +0300

    Update RayCast.cs

--- Assets/Scripts/RayCast.cs
+++ Assets/Scripts/RayCast.cs
@@ -6,73 +6,28 @@ public class RayCast : MonoBehaviour
     public Vector3 pos;
     public GameObject[] Towers;
     public GameObject Tower;
+    public LayerMask LayerTower;
+    public LayerMask LayerPoint;
 
 
 
     void Update()
     {
         Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+        RaycastHit2D hitTower = Physics2D.Raycast(worldPoint, Vector2.zero, 100f, LayerTower);
+        RaycastHit2D hitPoint = Physics2D.Raycast(worldPoint, Vector2.zero, 100f, LayerPoint);
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && hitTower.collider != null && Tower == null)
         {
-            if (hit.collider != null)
-            {
-                Debug.Log(hit.collider.name);
-            }
+            Destroy(hitTower.collider.gameObject);
         }
 
-        if (Input.GetMouseButtonDown(0) && hit.collider != null)
+        if (Input.GetMouseButtonDown(0) && hitTower.collider == null && hitPoint.collider != null && Tower != null)
         {
-            if (hit.collider.gameObject.layer == 8 && Tower == null)
-            {
-                Destroy(hit.collider.gameObject);
-            }
-            if (hit.collider.gameObject.layer == 7 && Tower != null)
-            {
-                pos = hit.collider.gameObject.transform.position;
-                Instantiate(Tower, pos, Tower.transform.rotation);
-            }
+            pos = hitPoint.collider.gameObject.transform.position;
+            Instantiate(Tower, pos, Tower.transform.rotation);
         }
 
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    if (Tower == null)
-        //    {
-        //        if ((hit.collider.gameObject.tag == "AttackRangeTower1" || hit.collider.gameObject.tag == "AttackRangeTower2" || hit.collider.gameObject.tag == "AttackRangeTower3"))
-        //        {
-        //            Destroy(hit.collider.gameObject);
-        //        }
-        //    }
-        //    if (hit.collider.gameObject.tag == "Tower Position")
-        //    {
-
-        //        if (Tower == null)
-        //        {
-        //            if (hit.collider.gameObject.tag == "AttackRangeTower1" || hit.collider.gameObject.tag == "AttackRangeTower2" || hit.collider.gameObject.tag == "AttackRangeTower3")
-        //            {
-        //                Destroy(hit.collider.gameObject);
-        //            }
-        //        }
-        //        else if (hit.collider.gameObject.tag == "AttackRangeTower1")
-        //        {
-        //            return;
-        //        }
-        //        else if (hit.collider.gameObject.tag == "AttackRangeTower2")
-        //        {
-        //            return;
-        //        }
-        //        else if (hit.collider.gameObject.tag == "AttackRangeTower3")
-        //        {
-        //            return;
-        //        }
-        //        else
-        //        {
-        //            pos = hit.collider.gameObject.transform.position;
-        //            Instantiate(Tower, pos, Tower.transform.rotation);
-        //        }
-        //    }
-        //}
     }
 
 
