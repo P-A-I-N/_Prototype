9182dc690acc2baf662ce115c5a0665a1eb3dc66
alexmos2 <alexmos67@yandex.ru>
2021-04-22 18:54:08 +0300

    +

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -55,6 +55,7 @@ public class SpawnEnemy : MonoBehaviour
         if (waveTextPrint && Time.time > timeTextPrint)
         {
             WaveText.text = "";
+            waveTextPrint = false;
         }
     }
 
