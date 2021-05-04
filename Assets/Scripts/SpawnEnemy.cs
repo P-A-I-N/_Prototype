9090d3a46fb2e509f2f3211a9aad89683e51412c
alexmos2 <alexmos67@yandex.ru>
2021-05-04 16:56:17 +0300

    +

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using System;
 
 public class SpawnEnemy : MonoBehaviour
 {
     public GameObject[] enemy;
     public int numberOfEnemies;
-    public int randomRangeSpawn;
     public int randomRangeEnemies;
+    int minTimeSpawn = 10;
+    int maxTimeSpawn = 10;
     public float spawnTime; 
     public float  spawnTimeRND;
     public bool endOfTheWave;
@@ -20,33 +22,39 @@ public class SpawnEnemy : MonoBehaviour
     public UnityEngine.UI.Text WaveText;
     bool waveTextPrint = false;
     float timeTextPrint;
+    float[] timeWaves = new float[26] {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+    float endWaveTime;
+    bool waveIsActive = true;
 
     void Start()
     {
         if (randomRangeEnemies < 1) randomRangeEnemies = 100;
-        if (randomRangeSpawn < 1) randomRangeSpawn = 100;
-        numberOfEnemies = Random.Range (numberOfEnemies - numberOfEnemies / randomRangeEnemies, numberOfEnemies + numberOfEnemies / randomRangeEnemies);
-        spawnTimeRND = Random.Range (spawnTime - spawnTime / randomRangeSpawn, spawnTime + spawnTime / randomRangeSpawn);
+        numberOfEnemies = UnityEngine.Random.Range (numberOfEnemies - numberOfEnemies / randomRangeEnemies, numberOfEnemies + numberOfEnemies / randomRangeEnemies);
+        spawnTimeRND = UnityEngine.Random.Range (minTimeSpawn, maxTimeSpawn);
         exit_time = Time.time + spawnTimeRND;
         WaveText.text = "Wave " + current_wave;
         waveTextPrint = true;
-        timeTextPrint = Time.time + 5;
-        SettingWave();
+        timeTextPrint = Time.time + 5; 
+        endWaveTime = Time.time + timeWaves[0];
+        string [] waves = wavesTable.text.Split ('\n');
+        SettingWave(waves [0]);
         //InvokeRepeating("CreateEnemy", spawnTime, spawnTime);
         //InvokeRepeating("summ", spawnTime, spawnTime);
     }
     void Update()
     {
-        if (Time.time >= exit_time && !endOfTheWave)
+        if (Time.time >= exit_time && !endOfTheWave && sum < numberOfEnemies && waveIsActive)
         {
             CreateEnemy();
-            summ();
+            sum++;
         }
         if (endOfTheWave && current_wave < num_waves)
         {
+            endWaveTime = Time.time + timeWaves[current_wave];
             current_wave++;
             endOfTheWave = false;
-            SettingWave();
+            string[] waves = wavesTable.text.Split('\n');
+            SettingWave( waves[current_wave - 1]) ;
             sum = 0;
             WaveText.text = "Wave " + current_wave;
             waveTextPrint = true;
@@ -57,28 +65,33 @@ public class SpawnEnemy : MonoBehaviour
             WaveText.text = "";
             waveTextPrint = false;
         }
-    }
-
-    void SettingWave ()
-    {
-
-    }
-    void summ()
-    {
-        if (sum < numberOfEnemies)
+        if (Time.time > endWaveTime)
         {
-            sum++;
+            endOfTheWave = true;
         }
-        else endOfTheWave = true;
+    }
+
+    void SettingWave (string wave)
+    {
+        if (wave == "n")
+        {
+            waveIsActive = false;
+            return;
+        }
+        else waveIsActive = true;
+        string[] parametrs = wave.Split(' ');
+        numberOfEnemies = Convert.ToInt32 (parametrs[0]);
+        minTimeSpawn = Convert.ToInt32 (parametrs[1]);
+        maxTimeSpawn = Convert.ToInt32 (parametrs[2]);
     }
 
     void CreateEnemy()
     {
         if (sum < numberOfEnemies)
         {
-            e = Instantiate(enemy[Random.Range (0, enemy.Length)], transform.position, transform.rotation);
+            e = Instantiate(enemy[UnityEngine.Random.Range (0, enemy.Length)], transform.position, transform.rotation);
             e.transform.SetParent(parent);
-            spawnTimeRND = Random.Range (spawnTime - spawnTime / randomRangeSpawn, spawnTime + spawnTime / randomRangeSpawn);
+            spawnTimeRND = UnityEngine.Random.Range (minTimeSpawn, maxTimeSpawn);
             exit_time = Time.time + spawnTimeRND;
         }
     }
