6170f15fa161956c6795536d901783cf6fb78d31
alexmos2 <alexmos67@yandex.ru>
2021-05-19 14:56:46 +0300

    +

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -4,14 +4,12 @@ using System;
 public class SpawnEnemy : MonoBehaviour
 {
     public GameObject[] enemy;
-    int numberOfEnemies;
     int minTimeSpawn = 10;
     int maxTimeSpawn = 10;
     float spawnTimeRND;
     public bool endOfTheWave;
     float exit_time;
     public Transform parent;
-    private int sum;
     GameObject e;
     int num_waves = 9;
     int current_wave = 1;
@@ -19,11 +17,19 @@ public class SpawnEnemy : MonoBehaviour
     public UnityEngine.UI.Text WaveText;
     bool waveTextPrint = false;
     float timeTextPrint;
+<<<<<<< Updated upstream
     float[] timeWaves = new float[26] {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
     float endWaveTime;
     bool waveIsActive = true;
     public GameObject waveScreen;
     int[] enemiesCount = new int[5] { 2,2,2,2,2 };
+=======
+    float endWaveTime;
+    bool waveIsActive = true;
+    public GameObject waveScreen;
+    public int firstEncounterWave;
+    public WaveForm waveForm;
+>>>>>>> Stashed changes
 
     void Start()
     {
@@ -31,25 +37,31 @@ public class SpawnEnemy : MonoBehaviour
         WaveText.text = "Wave " + current_wave;
         waveTextPrint = true;
         timeTextPrint = Time.time + 5; 
+<<<<<<< Updated upstream
         endWaveTime = Time.time + timeWaves[0];
+=======
+>>>>>>> Stashed changes
         string [] waves = wavesTable.text.Split ('\n');
         SettingWave(waves [0]);
     }
     void Update()
     {
-        if (Time.time >= exit_time && !endOfTheWave && sum < numberOfEnemies && waveIsActive)
+        if (Time.time >= exit_time && !endOfTheWave && waveForm.sum < waveForm.numberOfEnemies[current_wave - 1] && waveIsActive)
         {
             CreateEnemy();
-            sum++;
+            waveForm.sum++;
         }
         if (endOfTheWave && current_wave < num_waves)
         {
+<<<<<<< Updated upstream
             endWaveTime = Time.time + timeWaves[current_wave];
+=======
+>>>>>>> Stashed changes
             current_wave++;
             endOfTheWave = false;
             string[] waves = wavesTable.text.Split('\n');
             SettingWave( waves[current_wave - 1]) ;
-            sum = 0;
+            waveForm.sum = 0;
             WaveText.text = "Wave " + current_wave;
             waveTextPrint = true;
             timeTextPrint = Time.time + 5;
@@ -70,6 +82,7 @@ public class SpawnEnemy : MonoBehaviour
     {
         if (wave == "n")
         {
+<<<<<<< Updated upstream
             waveIsActive = false;
             return;
         }
@@ -83,36 +96,44 @@ public class SpawnEnemy : MonoBehaviour
         numberOfEnemies = Convert.ToInt32 (parametrs[5]);
         minTimeSpawn = Convert.ToInt32 (parametrs[6]);
         maxTimeSpawn = Convert.ToInt32 (parametrs[7]);
+=======
+            endWaveTime = Time.time + Convert.ToInt32 (parametrs[27]);
+            waveIsActive = false;
+            return;
+        }
+        else waveIsActive = true; 
+        minTimeSpawn = Convert.ToInt32 (parametrs[25]);
+        maxTimeSpawn = Convert.ToInt32 (parametrs[26]);
+        endWaveTime = Time.time + Convert.ToInt32 (parametrs[27]);
+        spawnTimeRND = UnityEngine.Random.Range(minTimeSpawn, maxTimeSpawn);
+        exit_time = Time.time + spawnTimeRND;
+>>>>>>> Stashed changes
     }
 
     void CreateEnemy()
     {
         int tryIndex, enemyIndex;
-        if (sum < numberOfEnemies)
+        if (waveForm.sum < waveForm.numberOfEnemies [current_wave - 1])
         {
-            //print(enemiesCount[0] + enemiesCount[1] + enemiesCount[2] + enemiesCount[3] + enemiesCount[4]);
-            if (enemiesCount[0] + enemiesCount[1] + enemiesCount[2] + enemiesCount[3] + enemiesCount[4] > 0)
-            {
                 while (true)
                 {
                     tryIndex = UnityEngine.Random.Range(0, enemy.Length);
-                    if (enemiesCount[tryIndex] > 0)
+                    if (waveForm.enemiesCount[current_wave - 1, tryIndex] > 0)
                     {
                         enemyIndex = tryIndex;
-                        enemiesCount[enemyIndex]--;
+                        waveForm.enemiesCount[current_wave - 1, enemyIndex]--;
                         break;
                     }
                 }
-            }
-            else
-            {
-                waveIsActive = false;
-                return; 
-            }
-            e = Instantiate(enemy[enemyIndex], transform.position, transform.rotation);
-            e.transform.SetParent(parent);
-            spawnTimeRND = UnityEngine.Random.Range (minTimeSpawn, maxTimeSpawn);
-            exit_time = Time.time + spawnTimeRND;
         }
+        else
+        {
+            waveIsActive = false;
+            return; 
+        }
+        e = Instantiate(enemy[enemyIndex], transform.position, transform.rotation);
+        e.transform.SetParent(parent);
+        spawnTimeRND = UnityEngine.Random.Range (minTimeSpawn, maxTimeSpawn);
+        exit_time = Time.time + spawnTimeRND;
     }
 }
